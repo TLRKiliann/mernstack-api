82e4f5f4ea1072ed97f3f9a1fbed0794ea36ec20
TLRKiliann <philogenie@protonmail.com>
2022-11-08 10:29:05 +0100

    component added

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,44 @@
 import { Component } from 'react'
+import notesType from './Model/notesType'
+import {Notes} from './notesModel'
+import FirstNotes from './components/FirstNotes'
+import LastNotes from './components/LastNotes'
+import AgeNotes from './components/AgeNotes'
+import LocalNotes from './components/LocalNotes'
 import './App.scss'
 
 
-class App extends Component<{}> {
+class App extends Component<notesType> {
+  state = {
+    Notes: []
+  }
+
+  componentDidMount() {
+    this.setState({Notes})
+    console.log("Mounted !")
+  }
+
   render() {
     return (
       <div className="App">
-        Hello
+        <h1>Notes MERN-stack API</h1>
+        {this.state.Notes.map(n => (
+          <div key={n.id}>
+            <FirstNotes
+              name={n.name}
+            />
+            <LastNotes
+              lastname={n.lastname}
+            />
+            <AgeNotes
+              age={n.age}
+            />
+            <LocalNotes
+              location={n.location}
+            />
+          </div>
+          ))
+        }
       </div>
     )
   }
