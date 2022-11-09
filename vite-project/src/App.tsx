cf953a23c273f447de64b93939ae2c9ddf74a000
TLRKiliann <philogenie@protonmail.com>
2022-11-09 19:25:52 +0100

    style & code updated

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -11,12 +11,24 @@ export default class App extends Component<{}> {
   state = {
     notes: [],
     textHeader: "Text Header",
-    secondTextHeader: "MY SECOND TEXT HEADER"
+    secondTextHeader: "MY SECOND TEXT HEADER",
+    isClosed: false,
+    isLefted: false
   }
 
   componentDidMount() {
     console.log("Mounted !")
     this.setState({notes: db_notes})
+  }
+
+  handleVoletsRight = () => {
+    console.log(this.state.isClosed)
+    this.setState({isClosed: !this.state.isClosed})
+  }
+
+  handleVoletsLeft = () => {
+    console.log(this.state.isLefted)
+    this.setState({isLefted: !this.state.isLefted})
   }
 
   render() {
@@ -28,15 +40,48 @@ export default class App extends Component<{}> {
         </div>
         
         <div className="btn--voletsLeft">
-          <button>
+          <button onClick={this.handleVoletsLeft}>
             Click me(L)
           </button>
         </div>
         <div className="btn--voletsRight">
-          <button>
+          <button onClick={this.handleVoletsRight}>
             Click me(R)
           </button>
         </div>
+
+
+        {this.state.isClosed ? (
+          <div className="volet--R">
+            <div className="subvolet--R">
+              <button onClick={this.handleVoletsRight}>Close</button>
+              <p>Volet R 1</p>
+            </div>
+          </div>
+          ) : (
+          <div className="volet--R2">
+            <div className="subvolet--R2">
+              Volet R 2
+            </div>
+          </div>
+          )
+        }
+
+        {this.state.isLefted ? (
+          <div className="volet--L">
+            <div className="subvolet--L">
+              <p>Volet L 1</p>
+              <button onClick={this.handleVoletsLeft}>Close</button>
+            </div>
+          </div>
+          ) : (
+          <div className="volet--L2">
+            <div className="subvolet--L2">
+              Volet L 2
+            </div>
+          </div>
+          )
+        }
 
         <Header />
         <MainTitle
