131b32d84397bd435b8f09cc23d7319e320e18c5
TLRKiliann <philogenie@protonmail.com>
2022-11-11 13:20:04 +0100

    new branch for right volet

--- vite-project/src/Pages/Volets/VoletRight.tsx
+++ vite-project/src/Pages/Volets/VoletRight.tsx
@@ -1,4 +1,6 @@
 import { Component } from 'react'
+import { Link } from 'react-router-dom'
+import mother from '../../assets/motherboard.png'
 import './VoletRight.scss'
 
 
@@ -16,6 +18,127 @@ export default class VoletRight extends Component<VoletRightProps> {
             <div className="subvolet--R">
               <button onClick={this.props.handleVoletsRight}>X</button>
               <p>Volet R 1</p>
+              <img src={mother} width='100px' alt='no img volet-R 1' />
+              <section>
+                <h1>Links</h1>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}
+                  >
+                    Hard Drive
+                  </Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>CPU</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>RAM</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Drivers</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Systemd</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Process</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>internet</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Client-Serveur</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Internet Protocol</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Request - Response</Link>
+                </li>
+
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>SSH - SCP</Link>
+                </li>
+
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>HTTP - HTTPS</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Fire-Wall</Link>
+                </li>
+                <li className="li">
+                  <Link
+                    to='/linksvoletright'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'lightgreen'
+                    }}>Proxy - VPN</Link>
+                </li>
+              </section>
             </div>
           </div>
           ) : (
