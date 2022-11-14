a3f9b7e70c87d2ee083619bdabb004fdcc29b963
TLRKiliann <philogenie@protonmail.com>
2022-11-14 18:23:28 +0100

    Pages renamed to pages

--- vite-project/src/Pages/Volets/__tests__/VoletLeft.test.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import {assert, assertType, expectTypeOf, beforeEach, afterEach, describe, expect, test, it, vi} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
-import VoletLeft from "../VoletLeft.tsx";
