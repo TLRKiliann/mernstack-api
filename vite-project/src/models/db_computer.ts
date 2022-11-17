26774b4e9ab9e75d40c9dea22617efe7e54912ba
TLRKiliann <philogenie@protonmail.com>
2022-11-17 12:12:59 +0100

    new reusable component for /chatcomputer (at work)

---
+++ vite-project/src/models/db_computer.ts
@@ -0,0 +1,62 @@
+export const computers = [
+	{
+		'id': 1,
+		'title': "Hard Disk - SSD",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 2,
+		'title': "CPU",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 3,
+		'title': "RAM",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 4,
+		'title': "Drivers",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 5,
+		'title': "Systemd",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 6,
+		'title': "Process",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 7,
+		'title': "Client-Server",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 8,
+		'title': "Protocols",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 9,
+		'title': "Request - Response",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 10,
+		'title': "Fire-Wall",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 11,
+		'title': "Proxy - VPN",
+		'img_bg': '../assets/world_bin.png'
+	},
+	{
+		'id': 12,
+		'title': "Cyber-Security",
+		'img_bg': '../assets/world_bin.png'
+	},
+]
