72755497caac949c1573b3a73a94385fe8182c51
TLRKiliann <philogenie@protonmail.com>
2022-11-22 13:53:25 +0100

    animation setup & rest at work

--- vite-project/src/components/ComputerRoom.tsx
+++ vite-project/src/components/ComputerRoom.tsx
@@ -1,22 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { db_users } from '../models/db_users'
+import { userType } from '../models/userType'
 import './styleComponents/ComputerRoom.scss'
 
-interface ComputerRoomProps {
-  link: string
-}
-
-const ComputerRoom: React.FC = ({props}: ComputerRoomProps) => {
+const ComputerRoom: React.FC = () => {
 
   const params = useParams<{ link?: object }>()
-  console.log("{params}", typeof params)
+  console.log("params", params)
+
+  const [users, setUsers] = useState<Array<userType>>([])
+  const [roomStyle, setRoomStyle] = useState<object>(Object.values(params))
+  console.log(roomStyle, "roomStyle")
+
+  useEffect(() => {
+    setUsers(db_users)
+    switch(roomStyle[0]) {
+      case 'RAM 4GB 8GB 16GB':
+        setRoomStyle("Gigabyte")
+        break
+      default:
+        console.log("End of loop")
+        break
+    }
+  }, [])
 
   return(
     <div className="nextcomp--room">
+
+      <div className={`div--animation${roomStyle}`}>
+        <div className="rotation--roomstyle">
+          <h1>{roomStyle}</h1>
+        </div>
+      </div>
+
       <h1>Room {Object.values(params)}</h1>
-      <p>{props}</p>
+      
+      <div className="div--terminaluser">
+        <textarea className="textarea--terminal">
+        </textarea>
+        <section className="user--online">
+          {Object.values(users).map((val, key) => (
+            <p key={key}>
+              {val.firstName} {val.isConnected}
+            </p>
+            ))
+          }
+        </section>
+      </div>
+
     </div>
   )
 }
 
 export default ComputerRoom;
+
+/*
+    switch(params) {
+      case 'RAM 4GB 8GB 16GB':
+        console.log("ok !!!!!!!!!!!!!")
+        break
+      default:
+        console.log("End of loop")
+        break
+    }
+*/
