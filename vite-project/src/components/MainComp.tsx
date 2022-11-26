3413ff12629d4a76883fcee8486101c6ccac176d
TLRKiliann <philogenie@protonmail.com>
2022-11-26 23:42:17 +0100

    aligned with component above

--- vite-project/src/components/MainComp.tsx
+++ vite-project/src/components/MainComp.tsx
@@ -14,47 +14,41 @@ const MainComp: React.FC = (props: MainCompProps) => {
 
   return(
     <div className="master--maincomp">
-
-      <div className="submaster--maincomp">
           
-        <div className="bg--colormaincomp">
+      <div className="bg--colormaincomp">
 
-          <h2 className='h2--maincomp'>
-            Member Connected !
-          </h2>
+        <h2 className='h2--maincomp'>
+          Member Connected !
+        </h2>
 
-          <div>
-
-            {props.users.slice(0, 10).map(user => (
-              <div key={user.id}>
-              {user.isConnected ? (
-                <div className="div--connection">
-                  <img
-                    src={user.img}
-                    width="40px"
-                    height="40px"
-                    className="img--connector"
-                    alt={user.img}
-                  />
-                  {user.firstName} {user.lastName} :&nbsp;
-                    <div 
-                      style={{
-                        fontSize: '1.1rem',
-                        fontWeight: 'bold',
-                        color: 'lightgreen'
-                      }}>
-                      "Connected"
-                    </div>
-                </div>
-                ) : null
-              }
+        <div>
+          {props.users.slice(0, 10).map(user => (
+            <div key={user.id}>
+            {user.isConnected ? (
+              <div className="div--connection">
+                <img
+                  src={user.img}
+                  width="40px"
+                  height="40px"
+                  className="img--connector"
+                  alt={user.img}
+                />
+                {user.firstName} {user.lastName} :&nbsp;
+                  <div 
+                    style={{
+                      fontSize: '1.1rem',
+                      fontWeight: 'bold',
+                      color: 'lightgreen'
+                    }}>
+                    "Connected"
+                  </div>
               </div>
-              ))
+              ) : null
             }
-
-          </div>
+            </div>
+            ))
+          }
         </div>
-
       </div>
     </div>
   )
