dba7464ee2b8070036aea47ef47d378aac992540
TLRKiliann <philogenie@protonmail.com>
2022-12-05 19:05:00 +0100

    more info about invitation to UI

--- vite-project/src/components/ResponsePrivateMsg.tsx
+++ vite-project/src/components/ResponsePrivateMsg.tsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react'
 
+
+interface db_usersProps {
+  db_users: Array<UserType>
+}
+
+type Field = {
+  value?: string
+}
+
+type Form = {
+  invite: Field
+}
+
 interface ResponsePrivateMsgProps {
+  form: object
+  catchById: db_usersProps
   handleCloseResponse: () => void
 }
 
-const ResponsePrivateMsg: React.FC = (props: ResponsePrivateMsgProps) => {
+const ResponsePrivateMsg: React.FC = (props: {
+  ResponsePrivateMsgProps,
+  db_usersProps,
+  Form,
+  Field}) => {
 
   return(
     <div className="response--private">
       <section className="section--response">
-        <h2 className="title--response">State of Invitation</h2>
-        <p className="p--response">Message sent !</p>
+        <h2 className="title--response">
+          State of <span style={{color: 'orange'}}>{props.form}</span>
+        </h2>
+        <p className="p--response">Message sent to&nbsp;
+          <span className="span--response">{props.catchById.firstName}!</span>
+        </p>
         <button
           onClick={props.handleCloseResponse}
           className="btn--response"
