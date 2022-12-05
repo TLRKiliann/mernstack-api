28ff365ce23e7f0504a76adcc8200fe52cf59456
TLRKiliann <philogenie@protonmail.com>
2022-12-05 01:06:26 +0100

    delete :firstname from PrivateMessage route

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -43,7 +43,7 @@ const App:React.FC = () => {
           </PrivateRoute>
         } />
 
-        <Route path='/computerroom/privatemessage/:firstName' element={
+        <Route path='/computerroom/privatemessage' element={
           <PrivateRoute>
             <PrivateMessage />
           </PrivateRoute>
