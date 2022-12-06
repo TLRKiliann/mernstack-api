004bf57440e7e07836b65a4cb372b4a3b4d115ba
TLRKiliann <philogenie@protonmail.com>
2022-12-06 16:58:06 +0100

    new route for Profile created

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar'
 import Login from './pages/Login'
 import Subscribe from './pages/Subscribe'
 import Home from './pages/Home'
+import Profile from './pages/Profile'
 import Online from './pages/Online'
 import Help from './pages/Help'
 import ChatComputer from './components/ChatComputer'
@@ -22,6 +23,11 @@ const App:React.FC = () => {
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/subscribe' element={<Subscribe />} />
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/profile' element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        } />
         <Route exact path='/online' element={
           <PrivateRoute>
             <Online />
