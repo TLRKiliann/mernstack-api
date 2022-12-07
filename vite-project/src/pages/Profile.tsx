b111facbd55c6b5dcc53c19eefa23b4df6c9abe8
TLRKiliann <philogenie@protonmail.com>
2022-12-07 12:11:02 +0100

    delete user function added, bar of items added, & style improved

--- vite-project/src/pages/Profile.tsx
+++ vite-project/src/pages/Profile.tsx
@@ -6,9 +6,12 @@ import '../stylePages/Profile.scss'
 
 const Profile: React.FC = () => {
 
-  const { username, tweekGroup } = useAuthLogin()
-
-  console.log(tweekGroup, "tweekGroup")
+  const { username, tweekGroup, setTweekGroup } = useAuthLogin()
+  //console.log(tweekGroup, "tweekGroup")
+  const handleDelete = (id: number) => {
+    setTweekGroup(tweekGroup.filter(t => t.id !== id))
+    alert(`Member deleted from your group`)
+  }
 
   return (
     <div className="profile">
@@ -24,7 +27,7 @@ const Profile: React.FC = () => {
 
       <div className="div--maintitle">
         <h1 className="title--profileh1">
-          Profile {username}
+          Profile of <span style={{color: 'orange'}}>{username}</span>
         </h1>
       </div>
 
@@ -32,8 +35,32 @@ const Profile: React.FC = () => {
         <div className="div--profileh2">
           <h2 className="titleh2--profile">Members of your group :</h2>
         </div>
+        
+        <div className="div--titleusersgroup">
+          <p className="p--profile0">
+          </p>
+          <p className="p--profile1">
+            Firstname
+          </p>
+          <p className="p--profile2">
+            Lastname
+          </p>
+          <p className="p--profile3">
+            Age
+          </p>
+          <p className="p--profile4">
+            Gender
+          </p>
+          <p className="p--profile5">
+            Email
+          </p>
+          <p className="p--profile6">
+            Location
+          </p>
+        </div>
+
         {Object.values(tweekGroup)?.map((val, key) => (
-          <div key={key} className="div--usersgroup">
+          <div key={val.id} className="div--usersgroup">
             <img
               src={val.img}
               width="40px"
@@ -41,12 +68,31 @@ const Profile: React.FC = () => {
               className="img--profileusers"
               alt={val.img}
             />
-            <p className="p--profile">{val.firstName}</p>
-            <p className="p--profile">{val.lastName}</p>
-            <p className="p--profile">{val.age}</p>
-            <p className="p--profile">{val.gender}</p>
-            <p className="p--profile">{val.email}</p>
-            <p className="p--profile">{val.location}</p>
+            <p className="p--profile1">
+              {val.firstName}
+            </p>
+            <p className="p--profile2">
+              {val.lastName}
+            </p>
+            <p className="p--profile3">
+              {val.age}
+            </p>
+            <p className="p--profile4">
+              {val.gender}
+            </p>
+            <p className="p--profile5">
+              {val.email}
+            </p>
+            <p className="p--profile6">
+              {val.location}
+            </p>
+            <button
+              type="button"
+              onClick={() => handleDelete(val.id)}
+              className="btn--deletefromgroup"
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
