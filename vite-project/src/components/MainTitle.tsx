bcff21f4b6235617849498b459df154cba3b0273
TLRKiliann <philogenie@protonmail.com>
2022-12-08 16:25:13 +0100

    img add behind title

--- vite-project/src/components/MainTitle.tsx
+++ vite-project/src/components/MainTitle.tsx
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import bamImg from '../assets/logo/bam-bg.png'
 import './styleComponents/MainTitle.scss'
 
 interface HeaderProps {
@@ -24,9 +25,18 @@ export default class Header extends Component<HeaderProps> {
   render() {
     return(
       <div className="maintitle">
-        <h1>Chat-Society</h1>
-        <h2>{this.props.textHeader}</h2>
-        <h4>{this.props.secondTextHeader}</h4>
+        <div className="div--bamimg">
+          <img
+            src={bamImg}
+            width="100%"
+            height="100%"
+            className="bam--img"
+            alt={bamImg}
+          />
+        </div>
+        <h1 className="main--h1">Chat-Society</h1>
+        <h2 className="main--h2">{this.props.textHeader}</h2>
+        <h4 className="main--h4">{this.props.secondTextHeader}</h4>
       </div>
     )
   }
