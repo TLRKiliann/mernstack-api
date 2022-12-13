6360b6e6ac8efb3b14ba7a242d00136e9d895052
TLRKiliann <philogenie@protonmail.com>
2022-12-13 13:14:37 +0100

    renamed file & code review

---
+++ vite-project/src/components/ConfirmInvitation.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+interface ConfirmInvitationProps {
+  isChecked: boolean
+  handleCheckBox: () => void
+  handleValidInvitation: (e: React.MouseEvent<HTMLButtonElement>) => void
+  //handleClose: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const ConfirmInvitation: React.FC = (props: ConfirmInvitationProps) => {
+  console.log(props, "props")
+  return (
+    <div className="box--confirminvitation">
+      <h4 className="confirm">Confirm invitation ?</h4>
+      <label>
+        <input
+          type="checkbox"
+          checked={props.isChecked}
+          onChange={props.handleCheckBox}
+          className="input--validationInvite"
+        />
+        yes
+      </label>
+
+      <button
+        type="button"
+        onClick={props.handleValidInvitation}
+        className="btn--validationlast"
+      >
+        Validate
+      </button>
+    </div>
+  )
+}
+
+export default ConfirmInvitation
