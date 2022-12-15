37d256f9fec1e154925f15ddb089e6afea787b5f
TLRKiliann <philogenie@protonmail.com>
2022-12-15 13:59:17 +0100

    class replaced by function (less code with useCustomHooks)

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -1,7 +1,7 @@
-import { Component } from 'react'
-import { db_users } from '../models/db_users'
+import { useState } from 'react'
+import usePersonnalHook from '../hook/personnal.hook'
+import useComputerHook from '../hook/computers.hook'
 import { UserType } from '../models/usertype'
-import { db_computers } from '../models/db_computers'
 import { computerType } from '../models/computerType'
 import MainTitle from '../components/MainTitle'
 import MainComp from '../components/MainComp'
@@ -11,101 +11,97 @@ import VoletLeft from './volets/VoletLeft'
 import cool from '../assets/background/chat-logobg.png'
 import '../stylePages/Home.scss'
 
-type DataAllType = {
-  db_users: UserType[]
-}
 
-export default class Home extends Component<DataAllType> {
-  state = {
-    users: [],
-    computers: [],
-    textHeader: "Login and Chat",
-    secondTextHeader: "(Only for Stack-Society Members)",
-    isOpenRight: false,
-    isOpenL: false
+const Home: React.FC = () => {
+  //Custom hooks
+  const users = usePersonnalHook()
+  const computers = useComputerHook()
+
+  const textHeader = "Login and Chat";
+  const secondTextHeader = "(Only for Stack-Society Members)";
+
+  const [isOpenRight, setIsOpenRight] = useState<boolean>(false)
+  const [isOpenL, setIsOpenL] = useState<boolean>(false)
+
+
+  const handleVoletsRight = () => {
+    setIsOpenRight(!isOpenRight)
   }
 
-  componentDidMount() {
-    this.setState({users: db_users})
-    this.setState({computers: db_computers})
+  const handleVoletsLeft = () => {
+    setIsOpenL(!isOpenL)
   }
 
-  handleVoletsRight = () => {
-    this.setState({isOpenRight: !this.state.isOpenRight})
-  }
+  return(
+    <div className="div--home">
 
-  handleVoletsLeft = () => {
-    this.setState({isOpenL: !this.state.isOpenL})
-  }
-
-  render() {
-    return(
-      <div className="div--home">
-
-        <div>
-          <img
-            src={cool}
-            width="1920px"
-            height="1080px"
-            className="img--home"
-            alt={cool}
-          />
-        </div>
-
-        <div className="wallIntro">
-          <h1 className="wallIntro--h1">Chat-Society</h1>
-        </div>
-
-        {!this.state.isOpenRight && <div className="div--voletsRight">
-            <button
-              onClick={this.handleVoletsRight}
-              className="btn--voletRight"
-            >
-              Computing
-            </button>
-          </div>
-        }
-
-        {!this.state.isOpenL && <div className="div--voletsLeft">
-            <button
-              onClick={this.handleVoletsLeft}
-              className="btn--voletLeft"
-            >
-              Chat-Room
-            </button>
-          </div>
-        }
-
-        <div className="animation--alternrota">
-          <div className="webdevanim">
-            <h1>Chat-Room</h1>
-          </div>
-
-          <div className="computinganim">
-            <h1>Computing</h1>
-          </div>
-        </div>
-
-        <VoletRight
-          computers={this.state.computers}
-          isOpenRight={this.state.isOpenRight}
-          handleVoletsRight={this.handleVoletsRight}
+      <div>
+        <img
+          src={cool}
+          width="1920px"
+          height="1080px"
+          className="img--home"
+          alt={cool}
         />
-
-        <VoletLeft
-          users={this.state.users}
-          isOpenL={this.state.isOpenL}
-          handleVoletsLeft={this.handleVoletsLeft} 
-        />
-
-        <MainTitle
-          textHeader={this.state.textHeader}
-          secondTextHeader={this.state.secondTextHeader}
-        />
-        <MainComp users={this.state.users} />
-        <Footer />
-
       </div>
-    )
-  }
+
+      <div className="wallIntro">
+        <h1 className="wallIntro--h1">Chat-Society</h1>
+      </div>
+
+      {!isOpenRight && <div className="div--voletsRight">
+          <button
+            onClick={handleVoletsRight}
+            className="btn--voletRight"
+          >
+            Computing
+          </button>
+        </div>
+      }
+
+      {!isOpenL && <div className="div--voletsLeft">
+          <button
+            onClick={handleVoletsLeft}
+            className="btn--voletLeft"
+          >
+            Chat-Room
+          </button>
+        </div>
+      }
+
+      <div className="animation--alternrota">
+        <div className="webdevanim">
+          <h1>Chat-Room</h1>
+        </div>
+
+        <div className="computinganim">
+          <h1>Computing</h1>
+        </div>
+      </div>
+
+      <VoletRight
+        computers={computers}
+        isOpenRight={isOpenRight}
+        handleVoletsRight={handleVoletsRight}
+      />
+
+      <VoletLeft
+        users={users}
+        isOpenL={isOpenL}
+        handleVoletsLeft={handleVoletsLeft} 
+      />
+
+      <MainTitle
+        textHeader={textHeader}
+        secondTextHeader={secondTextHeader}
+      />
+
+      <MainComp users={users} />
+      
+      <Footer />
+
+    </div>
+  )
 }
+
+export default Home
