2ce43da23b10589d09fb0ed9953d87b18c6e4d99
TLRKiliann <philogenie@protonmail.com>
2022-12-22 18:35:25 +0100

    new routes for privatechat terminal get + post

--- vite-project/src/services/serviceRouting.ts
+++ vite-project/src/services/serviceRouting.ts
@@ -18,10 +18,13 @@ const getUrl: string = "/api/getAllMembers";
 const postNewMember: string = "/api/createMembers";
 const updateNameRoom: string = '/api/updateRoom';
 const putInvite: string = '/api/inviteOtherUser';
-const putLastConfirm: string = '/api/confirmation';
+const putResponseUser: string = '/api/confirmationother';
 const putChgUsrRetConf: string = '/api/setUserConfirm';
+
 const postRouteMsgTerminal: string = '/api/msgTerminal';
 const retrieveMsgTerminal: string = '/api/retrieveMsgTerminal';
+const postPrivate: string = '/api/postprivate';
+const retrieveMsgPrivate: string = '/api/retrieveprivate';
 
 //GET all members of chat
 const getAllMembers = async () => {
@@ -75,7 +78,7 @@ const putInvitation = (id: number, dataForSigMsg: any) => {
   } 
 }
 
-//PUT username + returnConfirm
+//PUT username + signalRecieve + returnConfirm
 const updateUsrRetConf = (id: number, changeUserNameReturnConfirm: any) => {
   console.log(id, "id service")
   try {
@@ -91,10 +94,10 @@ const updateUsrRetConf = (id: number, changeUserNameReturnConfirm: any) => {
 }
 
 //PUT returnConfirm (db)
-const updateFinalConfirm = (id: number, changeReturnConf: any) => {
+const updateResponseUser = (id: number, changeReturnConf: any) => {
   console.log(id, "id")
   try {
-    const req = app.put<any>(`${putLastConfirm}/${id}`, changeReturnConf)
+    const req = app.put<any>(`${putResponseUser}/${id}`, changeReturnConf)
     return req.then((res: any) => res.data)
   } catch (err: any) {
     console.error("Error response PUT:");
@@ -128,15 +131,40 @@ const postMsgTerminal = (msgTerminal: string) => {
   }
 }
 
+const getMsgPrivate = async () => {
+  try {
+    const req = app.get<UserType>(retrieveMsgPrivate);
+    return await req.then((res: any) => res.data)
+  } catch (err: any) {
+    throw err;
+  }
+}
+
+const postMsgPrivate = (msgPrivate: string) => {
+  console.log(msgPrivate)
+  try {
+    const req = app.post<any>(postPrivate, msgPrivate)
+    return req.then((res: any) => res.data)
+  } catch (err: any) {
+    console.error("Error response PUT:");
+    console.error("erd", err.response.data);    // ***
+    console.error("ers", err.response.status);  // ***
+    console.error("erh", err.response.headers); // ***
+    throw err
+  }
+}
+
 const functionToCall = {
   getAllMembers,
   createMember,
   updateRoomName,
   putInvitation,
   updateUsrRetConf,
-  updateFinalConfirm,
+  updateResponseUser,
   getMsgTerminal,
-  postMsgTerminal
+  postMsgTerminal,
+  getMsgPrivate,
+  postMsgPrivate
 };
 
 export default functionToCall
