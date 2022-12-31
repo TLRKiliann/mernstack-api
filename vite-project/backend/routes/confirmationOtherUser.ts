2616c07ab75cdf73eb820949c501931de26085cd
TLRKiliann <philogenie@protonmail.com>
2022-12-31 18:27:26 +0100

    code review

--- vite-project/backend/routes/confirmationOtherUser.ts
+++ vite-project/backend/routes/confirmationOtherUser.ts
@@ -12,7 +12,7 @@ router.put('/', async (req: Request, res: Response, next: NextFunction) => {
   const room: string = req.body.room;
   const signalRecieve: boolean = req.body.signalRecieve;
   const returnConfirm: boolean = req.body.returnConfirm;
-  console.log(id, room, signalRecieve, returnConfirm, "confirmationother");
+  //console.log(id, room, signalRecieve, returnConfirm, "confirmationother");
   try {
     const result = await pool.query('update members set room=?, signalRecieve=?,\
       returnConfirm=? where id=?', [room, signalRecieve, returnConfirm, id]);
